59d9bc2811077f9e05cdaaab725a31ed055b4981
justin-cheng <84006554+chizhang977@users.noreply.github.com>
2024-05-05 15:04:02 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chizhang977/vitedoc.git master:gh-pages
+git push -f git@github.com:chizhang977/justin.git master:gh-pages
 
 
 
